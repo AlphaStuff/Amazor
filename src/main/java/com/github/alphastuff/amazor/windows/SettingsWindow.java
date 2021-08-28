0223ed1ce813ac75a32c111f7ecdd80e1f06c119
DomagojRatko <domagoj.ratko@gmail.com>
2021-08-28 22:04:43 +0200

    made change to settings gui

--- src/main/java/com/github/alphastuff/amazor/windows/SettingsWindow.java
+++ src/main/java/com/github/alphastuff/amazor/windows/SettingsWindow.java
@@ -3,10 +3,7 @@ import com.github.alphastuff.amazor.Amazor;
 import com.github.alphastuff.amazor.util.Checks;
 import com.github.alphastuff.amazor.util.ContentManager;
 
-import javax.print.DocFlavor;
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -26,13 +23,11 @@ public class SettingsWindow{
                 }
             };
 
-            frame.setSize(400, 400);
+            frame.setSize(200, 250);
             frame.setLocationRelativeTo(null);
 
             JCheckBox imageEnabled = new JCheckBox("Enable image showing", manager.isImageEnabled());
-            imageEnabled.addActionListener(e -> {
-                amazor.settings.set(ContentManager.IMAGE, imageEnabled.isSelected());
-            });
+            imageEnabled.addActionListener(e -> amazor.settings.set(ContentManager.IMAGE, imageEnabled.isSelected()));
             frame.add(imageEnabled);
 
             JList<String> imageType = new JList<>(new String[]{"cat", "shibe", "random"});
@@ -60,18 +55,54 @@ public class SettingsWindow{
             frame.add(imageAdvanced);
             frame.add(imageAdvancedUrl);
 
-            frame.setLayout(new FlowLayout());
+            frame.setLayout(null);
             frame.setFocusable(true);
 
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             frame.setAlwaysOnTop(true);
 
+            // ----------------------------------------------------------------------------
+            ImageIcon exitImg = new ImageIcon(new ImageIcon("src/main/resources/ePic.png").getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT));
+            JLabel exitPic = new JLabel();
+            exitPic.setBounds(170,3,25, 25);
+            exitPic.setIcon(exitImg);
+            frame.setUndecorated(true);
+            frame.setBackground(new Color(0.1f, 0.1f, 0.1f, 0.6f));
+            imageAdvanced.setBackground(new Color(0.1f, 0.1f, 0.1f, 0.3f));
+            imageAdvancedUrl.setBackground(new Color(0.1f, 0.1f, 0.1f, 0.3f));
+            imageEnabled.setBackground(new Color(0.1f, 0.1f, 0.1f, 0.3f));
+            imageType.setBackground(new Color(0.1f, 0.1f, 0.1f, 0.3f));
+            imageType.setForeground(Color.WHITE);
+            imageAdvanced.setForeground(Color.WHITE);
+            imageAdvancedUrl.setForeground(Color.WHITE);
+            imageEnabled.setForeground(Color.WHITE);
+
+            imageEnabled.setBounds(10,20,180,15);
+            imageAdvanced.setBounds(10,50,180,15);
+            imageAdvancedUrl.setBounds(10,80,180,15);
+            imageType.setBounds(10,110,180,100);
+
+            imageEnabled.setOpaque(false);
+            imageAdvanced.setOpaque(false);
+            imageAdvancedUrl.setOpaque(false);
+            imageType.setOpaque(false);
+
+
+            frame.getContentPane().add(exitPic);
             frame.setVisible(true);
 
             PopupWindow.FrameDragListener frameDragListener = new PopupWindow.FrameDragListener(frame);
             frame.addMouseListener(frameDragListener);
             frame.addMouseMotionListener(frameDragListener);
             frame.requestFocus();
+
+            exitPic.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    frame.dispose();
+                }
+            });
+
 
         };
         SwingUtilities.invokeLater(runnable);
